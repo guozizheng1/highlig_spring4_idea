83f803752718864f8bf8839a4e5f3392a350692d
Administrator <gzzwinner@163.com>
2018-07-15 11:22:09 +0800

    Spring Boot project commit local

--- src/main/java/com/wisely/highlight_spring4/ch1/aop/AopConfig.java
+++ src/main/java/com/wisely/highlight_spring4/ch1/aop/AopConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan("com.wisely.highlight_spring4.ch1.aop")
-@EnableAspectJAutoProxy //1
+@EnableAspectJAutoProxy //使用@EnableAspectJAutoProxy注解开启Spring对Aspect的支持
 public class AopConfig {
 
 }
--- src/main/java/com/wisely/highlight_spring4/ch1/aop/LogAspect.java
+++ src/main/java/com/wisely/highlight_spring4/ch1/aop/LogAspect.java
@@ -10,23 +10,24 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
-@Aspect//1
-@Component//2
+@Aspect//通过@Aspect注解声明一个切面
+@Component//通过@Component让此切面成为Spring容器管理的Bean
 public class LogAspect {
 
-    @Pointcut("@annotation(com.wisely.highlight_spring4.ch1.aop.Action)") //3
+    @Pointcut("@annotation(com.wisely.highlight_spring4.ch1.aop.Action)") //通过@PointCut注解声明切点
     public void annotationPointCut(){};
 
-    @After("annotationPointCut()") //4
+    @After("annotationPointCut()") //通过@After注解声明一个建言，并使用@PointCut定义的切点
     public void after(JoinPoint joinPoint){
         MethodSignature signature = (MethodSignature)joinPoint.getSignature();
         Method method = signature.getMethod();
         Action action = method.getAnnotation(Action.class);
-        System.out.println("注解式拦截 " + action.name()); //5
+        System.out.println("注解式拦截 " + action.name()); //通过反射可获得注解上的属性，然后做日志记录相关的操作，下面的相同
 
     }
 
-    @Before("execution(* com.wisely.highlight_spring4.ch1.aop.DemoMethodService.*(..))") //第一个*表示匹配任意的方法返回值，..(两个点)表示零个或多个，上面的第一个..表示service包及其子包,第二个*表示所有类,第三个*表示所有方法，第二个..表示方法的任意参数个数
+    @Before("execution(* com.wisely.highlight_spring4.ch1.aop.DemoMethodService.*(..))") //通过@Before注解声明一个建言，此建言直接使用拦截规则作为参数
+    // 第一个*表示匹配任意的方法返回值，..(两个点)表示零个或多个，上面的第一个..表示service包及其子包,第二个*表示所有类,第三个*表示所有方法，第二个..表示方法的任意参数个数
     public void before(JoinPoint joinPoint){
         MethodSignature signature = (MethodSignature)joinPoint.getSignature();
         Method method = signature.getMethod();
