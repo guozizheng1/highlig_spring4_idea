9ceb7d49c27ba15c2ac79a8b8d920dadbf359ade
gzzwinner <30675995+gzzwinner@users.noreply.github.com>
2018-07-20 18:35:28 +0800

    EL commit

---
+++ src/main/java/com/wisely/highlight_spring4/ch2/el/ELConfig.java
@@ -0,0 +1,11 @@
+package com.wisely.highlight_spring4.ch2.el;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+public class ELConfig {
+}
