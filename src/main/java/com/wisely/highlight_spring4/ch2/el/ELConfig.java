fbfed157d3be6c5dd6f5ded0b102b26ff182e0f0
gzzwinner <30675995+gzzwinner@users.noreply.github.com>
2018-08-17 18:10:59 +0800

    EL commit

--- src/main/java/com/wisely/highlight_spring4/ch2/el/ELConfig.java
+++ src/main/java/com/wisely/highlight_spring4/ch2/el/ELConfig.java
@@ -6,6 +6,60 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("com.wisely.highlight_spring4.ch2.el")
+@PropertySource("classpath:com/wisely/highlight_spring4/ch2/el/test.properties")
 
 public class ELConfig {
+
+    @Value("I love You!")
+    private String normal;
+
+    @Value("#{systemProperties['os.name']}")
+    private String osName;
+
+    @Value("#{T(java.lang.Math).random() * 100.0}")
+    private double randomNumber;
+
+    @Value("#{demoService.another}")
+    private String fromAnother;
+
+    @Value("classpath:com/wisely/highlight_spring4/ch2/el/test.txt")
+    private Resource testFile;
+
+    @Value("http://www.google.com")
+    private Resource testUrl;
+
+    @Value("${book.name}")
+    private String bookName;
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigure(){
+        return new PropertySourcesPlaceholderConfigurer();
+
+    }
+
+    public void outputResource(){
+        try{
+            System.out.println(normal);
+            System.out.println(osName);
+            System.out.println(randomNumber);
+            System.out.println(fromAnother);
+            System.out.println(IOUtils.toString(testFile.getInputStream()));
+            System.out.println(IOUtils.toString(testUrl.getInputStream()));
+            System.out.println(bookName);
+            System.out.println(environment.getProperty("book.author"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/wisely/highlight_spring4/ch2/el/Main.java
@@ -0,0 +1,15 @@
+package com.wisely.highlight_spring4.ch2.el;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Main {
+    public static void main(String args[]){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        ELConfig resourceService = context.getBean(ELConfig.class);
+
+        resourceService.outputResource();
+
+        context.close();
+    }
+}
